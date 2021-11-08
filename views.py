7516137baa7dcd8e673c2325ced4032ff97b7653
gadget8 <xta.uesugi@gmail.com>
2021-11-08 09:25:25 +0900

    ログイン機能実装

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import request, redirect, render_template, url_for
-from flask_bcrypt import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import login_required, login_user, logout_user
 from flask_login.utils import confirm_login
 from models import db, DailyReport, Branch
@@ -22,12 +22,12 @@ def login():
         password = request.form.get('password')
 
         branch = Branch.query.filter_by(login_name=login_name).first()
-        print(branch)
+        print(branch.branch_name)
         if branch and check_password_hash(branch.password, password):
             login_user(branch)
             return redirect(url_for('display'))
         else:
-            return redirect(url_for('login', login_name=login_name, password=password, err='ユーザー名かパスワードが間違っています'))
+            return render_template('login.html', error='ユーザー名かパスワードが間違っています')
 
     return render_template('login.html')
 
@@ -40,12 +40,13 @@ def create_branch():
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
         is_commissioner = request.form.get('is_commissioner')
-
+        print(request.form.get('is_commissioner'))
         if password != confirm_password:
-            return redirect(url_for('create_branch', branch_name=branch_name, login_name=login_name, password=password, confirm_password=confirm_password, is_commissioner=is_commissioner))
+            return render_template('create_branch.html', branch_name=branch_name, login_name=login_name, password=password, confirm_password=confirm_password, is_commissioner=is_commissioner, error='【要確認】パスワードと確認用パスワードが異なります')
         
         with db.session.begin(subtransactions=True):
-            new_branch = Branch(branch_name, login_name, password, is_commissioner)
+            new_branch = Branch(branch_name=branch_name, login_name=login_name, password=generate_password_hash(password, method='sha256'), is_commissioner=judge_commissioner(is_commissioner))
+            print(new_branch, branch_name, login_name, password, is_commissioner)
             db.session.add(new_branch)
         db.session.commit()
         return redirect(url_for('login'))
@@ -75,7 +76,8 @@ def report():
 
         readed = request.form['readed']
 
-        if request.form['discuss'] == '〇':
+        discuss = False
+        if request.form['discuss'] == 'True':
             discuss = True
 
         print(f'date:{date}, in_party:{in_party}, out_party:{out_party}, readed:{readed}, discuss:{discuss}')
@@ -87,11 +89,17 @@ def report():
         return redirect(url_for('display'))
     return render_template('report.html', today={'month':today.month, 'day':today.day}, days=calendar.monthrange(today.year, today.month)[1] + 1)
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user() # ログアウト
+    return redirect(url_for('login'))
+
 def confirm_match_password(password, confirm_password) -> bool:
-    if password == confirm_password:
-        return True
-    else:
-        return False
+    return True if password == confirm_password else False
+
+def judge_commissioner(is_commissioner) -> bool:
+    return True if is_commissioner == 'True' else False
 
 if __name__ == '__main__':
     app.run(debug=True)
